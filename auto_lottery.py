c40a8a07c9c0395954142bd5e8bc808ddc2be73b
Bonnie Schulkin <bonnie@hackbrightacademy.com>
2017-05-08 22:29:36 -0700

    added license

--- auto_lottery.py
+++ auto_lottery.py
@@ -1,5 +1,23 @@
 """Automated script to enter the hamilton lottery daily."""
 
+# Copyright (c) 2017 Bonnie Schulkin. All Rights Reserved.
+#
+# This file is part of hamlott.
+#
+# hamlott is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# hamlott is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+# for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with hamlott. If not, see <http://www.gnu.org/licenses/>.
+
+
 import os
 import sys
 import random
